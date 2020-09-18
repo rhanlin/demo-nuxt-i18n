8bd3478ea1ecb9c04ab34e0c413d3d89fd8f21bc
Spencer.Lin <spencer.lin@cloud-interactive.com>
2020-09-18 17:37:25 +0800

    update

--- i18n/shared/lang_zh_TW.js
+++
@@ -1,2 +0,0 @@
-/* eslint-disable */
-export default JSON.parse("{\"lang.zh_TW\":\"中文\",\"lang.en\":\"英文\",\"title\":\"nuxt-i18n 示範\",\"subtitle\":\"nuxt-i18n的演示\",\"documentation\":\"文件\",\"github\":\"GitHub\",\"home.btn\":\"前往首頁\",\"article.btn\":\"前往文章頁\"}")
